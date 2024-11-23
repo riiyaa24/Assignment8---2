0764571cc4b37b25fda8261f8e9fe53e05af01a6
Riya Dhanduke <113622644+riiyaa24@users.noreply.github.com>
2024-11-23 10:55:20 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ChildComponent, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent 
+{
+  public Message : any;
+}
